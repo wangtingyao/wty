f54071df80f238622afbe608ff1833e1925699cd
wty <wty2048@qq.com>
2022-03-08 20:06:35 +0800

    install

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo apt install -y cscope
+sudo apt install -y vim
+sudo apt install -y git
+sudo apt install -y ctags
+sudo apt install -y rtl8821ce-dkms
+sudo apt install -y net-tools
+sudo mkdir d
+sudo mount -t cifs //Pc-5cd133cmhr/d /mnt/d -o username=wangtingyao,password=Wty199048
+ln -s /mnt/d/lixiang lixiang
+#sudo vim /etc/fstab
+#//Pc-5cd133cmhr/d /mnt/d cifs auto,username=wangtingyao,password=Wty199048 0 0
+
+
+
+
+
