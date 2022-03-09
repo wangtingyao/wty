b73a9d38845a066abb8d5252fd3a896463427704
wty <wty2048@qq.com>
2022-03-09 11:01:59 +0800

    update install

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ sudo apt install -y git
 sudo apt install -y ctags
 sudo apt install -y rtl8821ce-dkms
 sudo apt install -y net-tools
+sudo apt install -y xrdp
+sudo apt install -y remmina
+sudo snap install gh
 sudo mkdir d
 sudo mount -t cifs //Pc-5cd133cmhr/d /mnt/d -o username=wangtingyao,password=Wty199048
 ln -s /mnt/d/lixiang lixiang
